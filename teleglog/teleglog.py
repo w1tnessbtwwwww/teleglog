4f101fce75613ed35aed086790b4f4a9d9914beb
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2025-01-07 03:01:00 +0700

    hotfixed brackets

--- teleglog/teleglog.py
+++ teleglog/teleglog.py
@@ -11,7 +11,7 @@ class TelegLog:
         self._bot.send_message(self._chat_id, template, parse_mode="Markdown")
 
     def info(self, message: str):
-        template = f"❗**INFO**❗\n{message}\nDate: {datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}"
+        template = f"❗**INFO**❗\n{message}\nDate: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}"
         
         sending = self.send_log(template)
         
@@ -19,20 +19,18 @@ class TelegLog:
     def error(self, message: str, traceback: Optional[str] = None) -> str:
 
         if traceback:
-            template = f"🚨 **ERROR** 🚨\n{message}\n```python\n{traceback}```\n"
-            template += f"Date: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}"
+            template = f"🚨 **ERROR** 🚨\n{message}\n```python\n{traceback}```\nDate: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}"
         else:
-            template = f"🚨 **ERROR** 🚨\n{message}\n"
-            template += f"Date: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}"
+            template = f"🚨 **ERROR** 🚨\n{message}\nDate: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}"
         
         sending = self.send_log(template)
         
     def warning(self, message: str) -> str:
-        template = f"⚠️ **WARNING** ⚠️\n{message}\nDate: {datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}"
+        template = f"⚠️ **WARNING** ⚠️\n{message}\nDate: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}"
         
         sending = self.send_log(template)
         
     def fatal(self, message: str) -> str:
-        template = f"🆘 **FATAL** 🆘\n{message}\nDate: {datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}"
+        template = f"🆘 **FATAL** 🆘\n{message}\nDate: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}"
         
         sending = self.send_log(template)
